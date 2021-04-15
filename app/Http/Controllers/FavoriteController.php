1c32753af324f2f4be5e6279ecf240fabf1fd403
b-forme-ogawa <ogawa@b-forme.net>
2021-04-15 11:14:17 +0900

    論理削除されているアイテムすべてがrestoreされる現象の修正

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -8,23 +8,32 @@ class FavoriteController extends Controller
 {
     public function index(){
      
-       $favorite = new Favorite();
-       $favorite->song = request()->input('song');
-       $favorite->album = request()->input('album');
-       $favorite->artist = request()->input('artist');
-       $favorite->jacket = request()->input('jacket');
-       $favorite->spotify_id = request()->input('spotify_id');
-       $favorite->spotify_url = request()->input('spotify_url');
+       $checkFavorite = new Favorite();
+
+       $favorite_already_exist = $checkFavorite->where('spotify_id', request()->input('spotify_id'))
+                                          ->whereNull('deleted_at')->first();
+
+       $favorite_already_deleted = Favorite::onlyTrashed()->where('spotify_id', request()->input('spotify_id'))->get();
        
-        if($favorite->where('spotify_url',$favorite->spotify_url)->exists()){
+        if( $favorite_already_exist){
             abort(403, 'すでに登録済みです');
             
         }
+        else if($favorite_already_deleted){
+            Favorite::onlyTrashed()->where('spotify_id', request()->input('spotify_id'))->restore();
+            
+        }
         else{
-            if(Favorite::onlyTrashed()->whereNotNull('id')->get()){
-                Favorite::onlyTrashed()->whereNotNull('id')->restore();
-            }
+            $favorite = new Favorite();
+            $favorite->song = request()->input('song');
+            $favorite->album = request()->input('album');
+            $favorite->artist = request()->input('artist');
+            $favorite->jacket = request()->input('jacket');
+            $favorite->spotify_id = request()->input('spotify_id');
+            $favorite->spotify_url = request()->input('spotify_url');
+
             $favorite->save();
+            return $favorite;
         }
     }
 
