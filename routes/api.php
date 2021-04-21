7f1015f86817549b4ae7ec5f50b18b385130ea93
b-forme-ogawa <ogawa@b-forme.net>
2021-04-21 15:55:48 +0900

    public,favoriteの分離

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/public','FavoriteController@index');
+Route::post('/public','PublicController@index');
+Route::get('/public','PublicController@getMusicData');
+Route::delete('/public/{id}', 'PublicController@deleteMusicData');
 
-Route::get('/public','FavoriteController@getMusicData');
-
-Route::delete('/favorite/{id}', 'FavoriteController@deleteMusicData');
 
 Route::post('/register', 'AuthController@userRegister');
-
 Route::post('/login', 'AuthController@login');
 
+Route::post('/favorite','FavoriteController@index');
+Route::get('/favorite','FavoriteController@getMusicData');
+Route::delete('/favorite/{id}', 'FavoriteController@deleteMusicData');
