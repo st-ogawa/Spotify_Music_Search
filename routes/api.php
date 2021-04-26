addaa523b4a62d65dbcf03c0018a7df950f316b3
b-forme-ogawa <ogawa@b-forme.net>
2021-04-26 15:55:08 +0900

    publicコントローラー統合

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/public','PublicController@index');
-Route::get('/public','PublicController@getMusicData');
-Route::delete('/public/{id}', 'PublicController@delete');
+Route::post('/public','FavoriteController@public_index');
+Route::get('/public','FavoriteController@get_public_music');
+Route::delete('/public/{id}', 'FavoriteController@delete_public_music');
 
 
 Route::post('/register', 'AuthController@userRegister');
 Route::post('/login', 'AuthController@login');
 
 Route::post('/favorite','FavoriteController@index');
-Route::get('/favorite','FavoriteController@getMusicData');
-Route::delete('/favorite/{id}', 'FavoriteController@deleteMusicData');
+Route::get('/favorite/{user_id}','FavoriteController@get_favorite_music');
+Route::delete('/favorite/{id}', 'FavoriteController@delete_favorite');
